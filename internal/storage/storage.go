51b86feb1e5bb65a162ddf51e4677143c54d43f1
jonandernovella <novella.uu@gmail.com>
2020-06-06 22:28:05 +0200

    Separate storage concerns
    S3 and Posix business has been separated with two different structs and
    they both implement the Backend interface.

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -7,10 +7,48 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Conf stores information about the storage backend
-type Conf struct {
-	Type string
-	// S3
+// Backend defines methods to be implemented by PosixReader and S3Reader
+type Backend interface {
+	ReadFile(filePath string) io.Reader
+}
+
+// PosixReader encapsulates an io.Reader instance
+type PosixReader struct {
+	FileReader io.Reader
+}
+
+// PosixConf stores information about the POSIX storage backend
+type PosixConf struct {
+	Location string
+	Mode     int
+	UID      int
+	GID      int
+}
+
+// NewPosixReader returns a PosixReader struct
+func NewPosixReader(c PosixConf) *PosixReader {
+	var reader io.Reader
+	return &PosixReader{FileReader: reader}
+}
+
+// ReadFile returns an io.Reader instance
+func (pr *PosixReader) ReadFile(filePath string) io.Reader {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Error(err)
+	}
+	pr.FileReader = file
+
+	return pr.FileReader
+}
+
+// S3Reader encapsulates a S3 reader instance
+type S3Reader struct {
+	ObjectReader io.Reader
+}
+
+// S3Conf stores information about the S3 storage backend
+type S3Conf struct {
 	URL       string
 	Port      int
 	AccessKey string
@@ -18,24 +56,14 @@ type Conf struct {
 	Bucket    string
 	Chunksize int
 	Cacert    string
-	// posix
-	Location string
-	Mode     int
-	UID      int
-	GID      int
 }
 
-// FileReader returns
-func FileReader(archive, filePath string) io.Reader {
-	var reader io.Reader
-	if archive == "s3" {
-		// s3 specifc stuff
-	} else {
-		file, err := os.Open(filePath)
-		if err != nil {
-			log.Error(err)
-		}
-		reader = file
-	}
-	return reader
+// NewS3Reader returns a S3Reader struct
+func NewS3Reader(c S3Conf) *S3Reader {
+	return &S3Reader{}
+}
+
+// ReadFile returns an io.Reader instance
+func (sr *S3Reader) ReadFile(filePath string) io.Reader {
+	return sr.ObjectReader
 }

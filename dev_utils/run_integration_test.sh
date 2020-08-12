8263e69d79d6a8d8e0be8f1dce9b56ce1583ec33
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2020-08-12 13:35:05 +0200

    Add shellscript to run integration test

---
+++ dev_utils/run_integration_test.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+for c in s3cmd jq
+do
+    if ! command -v $c
+    then
+        echo "$c could not be found"
+        exit 1
+    fi
+done
+
+
+FILE="dummy_data.c4gh"
+SHA="5e9c767958cc3f6e8d16512b8b8dcab855ad1e04e05798b86f50ef600e137578"
+MD5="b60fa2486b121bed8d566bacec987e0d"
+
+if [ "$1" != "" ]; then
+    SHA=$(sha256sum "$1" | awk '{print $1;}')
+    MD5=$(md5sum "$1" | awk '{print $1;}')
+    FILE="$(realpath "$1")"
+fi
+
+cd "$(dirname "$0")" || exit
+
+s3cmd -c s3cmd.conf put "$FILE" s3://inbox/test/dummy_data.c4gh
+
+curl -u test:test 'localhost:15672/api/exchanges/test/localega/publish' \
+-H 'Content-Type: application/json;charset=UTF-8' \
+--data-binary '{"vhost":"test","name":"localega","properties":{"delivery_mode":2,"correlation_id":"1","content_encoding":"UTF-8","content_type":"application/json"},"routing_key":"files","payload_encoding":"string","payload":"{\"type\":\"ingest\",\"user\":\"test\",\"filepath\":\"test/dummy_data.c4gh\",\"encrypted_checksums\":[{\"type\":\"sha256\",\"value\":\"'"$SHA"'\",\"type\":\"md5\",\"value\":\"'"$MD5"'\"}]}"}'
+
+RETRY_TIMES=0
+until docker logs --since 30s ingest 2>&1 | grep "Mark as archived"
+do 
+    echo "waiting for ingestion to complete"
+    RETRY_TIMES=$((RETRY_TIMES+1));
+    if [ $RETRY_TIMES -eq 6 ]; then
+        echo "Ingest failed"
+        exit 1
+    fi
+    sleep 10;
+done
+
+RETRY_TIMES=0
+until docker logs --since 30s verify 2>&1 | grep "Mark completed"
+do
+    echo "waiting for verification to complete"
+    RETRY_TIMES=$((RETRY_TIMES+1));
+    if [ $RETRY_TIMES -eq 6 ]; then
+        echo "Verification failed"
+        exit 1
+    fi
+    sleep 10;
+done
+
+message=$(curl -u test:test 'localhost:15672/api/queues/test/files.verified/get' \
+-H 'Content-Type: application/json;charset=UTF-8' \
+-d '{"count":1,"ackmode":"ack_requeue_true","encoding":"auto","truncate":50000}')
+
+decrypted_sha=$(echo "$message" | jq '.[0].payload | fromjson.decrypted_checksums[0].value' | cut -d '"' -f 2)
+decrypted_md5=$(echo "$message" | jq '.[0].payload | fromjson.decrypted_checksums[1].value' | cut -d '"' -f 2)
+file=$(echo "$message" | jq '.[0].payload | fromjson.filepath' | cut -d '"' -f 2)
+
+curl -u test:test 'localhost:15672/api/exchanges/test/localega/publish' \
+-H 'Content-Type: application/json;charset=UTF-8' \
+--data-binary '{"vhost":"test","name":"localega","properties":{"delivery_mode":2,"correlation_id":"1","content_encoding":"UTF-8","content_type":"application/json"},"routing_key":"stableIDs","payload_encoding":"string","payload":"{\"type\":\"accession\",\"user\":\"test\",\"filepath\":\"'"$file"'\",\"accession_id\":\"EGAF00123456789\",\"decrypted_checksums\":[{\"type\":\"sha256\",\"value\":\"'"$decrypted_sha"'\"},{\"type\":\"md5\",\"value\":\"'"$decrypted_md5"'\"}]}"}'
+
+RETRY_TIMES=0
+until docker logs finalize 2>&1 | grep "Mark ready"
+do
+    echo "waiting for finalize to complete"
+    RETRY_TIMES=$((RETRY_TIMES+1));
+    if [ $RETRY_TIMES -eq 6 ]; then
+        echo "Finalize failed"
+        exit 1
+    fi
+    sleep 10;
+done

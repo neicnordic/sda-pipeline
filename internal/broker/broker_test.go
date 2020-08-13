c7f0179d1fa59e6879427bc81504ac506aa1fab9
Stefan Negru <stefan@blankdots.com>
2020-08-13 20:16:49 +0300

    add broker unit tests
    add unit test for getmessages, sendmessage buildMqURI, confirmOne and NewMQ connection error

--- internal/broker/broker_test.go
+++ internal/broker/broker_test.go
@@ -1,37 +1,135 @@
 package broker
 
 import (
+	"bytes"
 	"fmt"
+	"sync"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
+	"github.com/stretchr/testify/assert"
 )
 
-type myChannel struct {
+type mockChannel struct {
 }
 
-func (*myChannel) Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args amqp.Table) (<-chan amqp.Delivery, error) {
-	fmt.Println("this is mychannel")
+func (*mockChannel) Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args amqp.Table) (<-chan amqp.Delivery, error) {
 	return nil, nil
 }
 
-func (*myChannel) Confirm(noWait bool) error {
+func (*mockChannel) Confirm(noWait bool) error {
 	return nil
 }
 
-func (*myChannel) NotifyPublish(confirm chan amqp.Confirmation) chan amqp.Confirmation {
+func (*mockChannel) NotifyPublish(confirm chan amqp.Confirmation) chan amqp.Confirmation {
 	return nil
 }
 
-func (*myChannel) Publish(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error {
+func (*mockChannel) Publish(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error {
 	return nil
 }
 
-func TestDialer(t *testing.T) {
+func (*mockChannel) Close() error {
+	return nil
+}
+
+func TestGetMessages(t *testing.T) {
 	b := AMQPBroker{}
-
-	c := myChannel{}
-
+	c := mockChannel{}
 	b.Channel = &c
-	GetMessages(&b, "hej")
+
+	GetMessages(&b, "queue")
+}
+
+func TestSendMessage(t *testing.T) {
+	b := AMQPBroker{}
+	c := mockChannel{}
+	b.Channel = &c
+	msg := []byte("Message")
+
+	err := SendMessage(&b, "corrID1", "exchange", "routingkey", false, msg)
+
+	if err != nil {
+		log.Errorf("Error testing send message: %s", err)
+	}
+}
+
+var tMqconf = Mqconf{"localhost",
+	5672,
+	"user",
+	"password",
+	"/vhost",
+	"queue",
+	"exchange",
+	"routingkey",
+	"routingError",
+	false,
+	false,
+	"cacert",
+	"clientcert",
+	"clientkey",
+	"servername",
+	true}
+
+func TestBuildMqURI(t *testing.T) {
+	amqps := buildMqURI("localhost", "user", "pass", "/vhost", 5555, true)
+	assert.Equal(t, "amqps://user:pass@localhost:5555/vhost", amqps)
+	amqp := buildMqURI("localhost", "user", "pass", "/vhost", 5555, false)
+	assert.Equal(t, "amqp://user:pass@localhost:5555/vhost", amqp)
+}
+
+func CatchNewMQPanic() (err error) {
+	// Recover if NewMQ panics
+	// Allow both panic and error return here, so use a custom function rather
+	// than assert.Panics
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			err = fmt.Errorf("Caught panic")
+		}
+	}()
+
+	_ = NewMQ(tMqconf)
+
+	return err
+}
+
+// Test connection refused as there is no running
+// broker
+func TestNewMQConn_Error(t *testing.T) {
+
+	brokerURI := buildMqURI(tMqconf.Host, tMqconf.User, tMqconf.Password, tMqconf.Vhost, tMqconf.Port, tMqconf.Ssl)
+
+	expectedMsg := "dial tcp [::1]:5672: connect: connection refused"
+
+	_, err := amqp.Dial(brokerURI)
+
+	assert.EqualError(t, err, expectedMsg)
+
+	newErr := CatchNewMQPanic()
+
+	if newErr == nil {
+		t.Errorf("New MQ did not report error when it should.")
+	}
+}
+
+func TestConfirmOne(t *testing.T) {
+
+	var str bytes.Buffer
+	log.SetOutput(&str)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	c := make(chan amqp.Confirmation)
+	go func(c <-chan amqp.Confirmation) {
+		confirmOne(c)
+		assert.NotZero(t, str.Len(), "Expected warnings were missing")
+		assert.Contains(t, str.String(), "failed delivery of delivery tag")
+		wg.Done()
+	}(c)
+	c <- amqp.Confirmation{}
+
+	wg.Wait()
 }

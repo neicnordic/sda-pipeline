00ab76af8d218c0584b51b11c2cf2f02c88d6bca
Pontus Freyhult <pontus.freyhult@nbis.se>
2020-08-18 15:49:03 +0200

    Tests for broker

--- internal/broker/broker_test.go
+++ internal/broker/broker_test.go
@@ -2,31 +2,65 @@ package broker
 
 import (
 	"bytes"
+	"crypto/tls"
 	"fmt"
+	"os"
 	"sync"
 	"testing"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 	"github.com/stretchr/testify/assert"
 )
 
-type mockChannel struct {
+const doesNotExist = "/does/not/exist"
+const readableFile = "broker.go"
+
+func TestMain(m *testing.M) {
+	logFatalf = testLogFatalf
+	code := m.Run()
+
+	os.Exit(code)
 }
 
-func (*mockChannel) Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args amqp.Table) (<-chan amqp.Delivery, error) {
+func testLogFatalf(f string, args ...interface{}) {
+	s := fmt.Sprintf(f, args...)
+	panic(s)
+}
+
+type mockChannel struct {
+	failConfirm    bool
+	confirmChannel chan amqp.Confirmation
+}
+
+func (c *mockChannel) Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args amqp.Table) (<-chan amqp.Delivery, error) {
 	return nil, fmt.Errorf("error")
 }
 
-func (*mockChannel) Confirm(noWait bool) error {
+func (c *mockChannel) QueueDeclarePassive(name string, durable, autoDelete, exclusive, noWait bool, args amqp.Table) (amqp.Queue, error) {
+	return amqp.Queue{}, fmt.Errorf("error")
+}
+
+func (c *mockChannel) Confirm(noWait bool) error {
+	if c.failConfirm {
+		return fmt.Errorf("failing for testing")
+	}
+
 	return nil
 }
 
-func (*mockChannel) NotifyPublish(confirm chan amqp.Confirmation) chan amqp.Confirmation {
-	return nil
+func (c *mockChannel) NotifyPublish(confirm chan amqp.Confirmation) chan amqp.Confirmation {
+	c.confirmChannel = confirm
+	return confirm
 }
 
-func (*mockChannel) Publish(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error {
+func (c *mockChannel) Publish(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error {
+	go func() {
+		time.Sleep(10000)
+		c.confirmChannel <- amqp.Confirmation{}
+	}()
+
 	return nil
 }
 
@@ -48,20 +82,44 @@ func TestGetMessages_Error(t *testing.T) {
 
 }
 
-func TestSendMessage_Error(t *testing.T) {
+func CatchSendMessage(b *AMQPBroker, corrID, exchange, routingkey string, reliable bool, body []byte) (err error) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			err = fmt.Errorf("Caught panic")
+		}
+	}()
+	err = SendMessage(b, corrID, exchange, routingkey, reliable, body)
+
+	return err
+}
+
+func TestSendMessage(t *testing.T) {
+
 	b := AMQPBroker{}
 	c := mockChannel{}
+
+	var err error
+	c.failConfirm = true
 	b.Channel = &c
 	msg := []byte("Message")
 
-	err := SendMessage(&b, "corrID1", "exchange", "routingkey", false, msg)
+	// Aborts run for some not yet understood reason
 
-	if err != nil {
-		log.Errorf("Error testing send message: %s", err)
-	}
+	err = CatchSendMessage(&b, "corrID1", "exchange", "routingkey", true, msg)
+	assert.NotNil(t, err, "Unexpected non-error from SendMessage (reliable)")
+
+	c.failConfirm = false
+
+	err = SendMessage(&b, "corrID1", "exchange", "routingkey", false, msg)
+	assert.Nil(t, err, "Unexpected error from SendMessage (not reliable)")
+
+	err = SendMessage(&b, "corrID1", "exchange", "routingkey", true, msg)
+	assert.Nil(t, err, "Unexpected error from SendMessage (reliable)")
+
 }
 
-var tMqconf = Mqconf{"localhost",
+var tMqconf = Mqconf{"127.0.0.1",
 	5672,
 	"user",
 	"password",
@@ -73,8 +131,8 @@ var tMqconf = Mqconf{"localhost",
 	true,
 	true,
 	"../../dev_utils/certs/ca.pem",
-	"../../dev_utils/certs/mq.pem",
-	"../../dev_utils/certs/mq-key.pem",
+	"../../dev_utils/certs/client.pem",
+	"../../dev_utils/certs/client-key.pem",
 	"servername",
 	true}
 
@@ -85,7 +143,55 @@ func TestBuildMqURI(t *testing.T) {
 	assert.Equal(t, "amqp://user:pass@localhost:5555/vhost", amqp)
 }
 
-func CatchNewMQPanic() (err error) {
+func TestNewMQ(t *testing.T) {
+	noSSLPort := 5555 // + int(rand.Float64()*32768)
+	sslPort := noSSLPort + 1
+
+	s := startServer(t, noSSLPort)
+
+	noSslConf := tMqconf
+	noSslConf.Ssl = false
+	noSslConf.VerifyPeer = false
+	noSslConf.Port = noSSLPort
+
+	go handleOneConnection(s.Sessions, false, false)
+	b := NewMQ(noSslConf)
+
+	assert.NotNil(t, b, "NewMQ without ssl did not return a broker")
+
+	// Fail the queuedeclarepassive
+	go handleOneConnection(s.Sessions, false, true)
+	errret := CatchNewMQPanic(t, noSslConf)
+	assert.NotNil(t, errret, "NewMQ did fail as expected")
+
+	// Fail the channel
+	go handleOneConnection(s.Sessions, true, true)
+	errret = CatchNewMQPanic(t, noSslConf)
+	assert.NotNil(t, errret, "NewMQ did fail as expected")
+
+	s.Close()
+
+	sslConf := tMqconf
+	sslConf.Ssl = true
+	sslConf.VerifyPeer = false
+	sslConf.Port = sslPort
+	sslConf.ServerName = sslConf.Host
+
+	serverTlsConfig := tlsServerConfig()
+	serverTlsConfig.ClientAuth = tls.NoClientCert
+
+	ss := startTLSServer(t, sslPort, serverTlsConfig)
+
+	go handleOneConnection(ss.Sessions, false, false)
+
+	b = NewMQ(sslConf)
+	assert.NotNil(t, b, "NewMQ with ssl did not return a broker")
+
+	ss.Close()
+
+}
+
+func CatchNewMQPanic(t *testing.T, conf Mqconf) (err error) {
 	// Recover if NewMQ panics
 	// Allow both panic and error return here, so use a custom function rather
 	// than assert.Panics
@@ -97,13 +203,13 @@ func CatchNewMQPanic() (err error) {
 		}
 	}()
 
-	noSslConf := tMqconf
-	noSslConf.Ssl = false
-	noSslConf.VerifyPeer = false
+	b := NewMQ(conf)
 
-	_ = NewMQ(noSslConf)
+	if b == nil {
+		return fmt.Errorf("NewMQ did not return a broker")
+	}
 
-	return err
+	return nil
 }
 
 // Test connection refused as there is no running
@@ -112,17 +218,35 @@ func TestNewMQConn_Error(t *testing.T) {
 
 	brokerURI := buildMqURI(tMqconf.Host, tMqconf.User, tMqconf.Password, tMqconf.Vhost, tMqconf.Port, tMqconf.Ssl)
 
-	expectedMsg := "dial tcp [::1]:5672: connect: connection refused"
+	expectedMsg := "dial tcp 127.0.0.1:5672: connect: connection refused"
 
 	_, err := amqp.Dial(brokerURI)
 
 	assert.EqualError(t, err, expectedMsg)
 
-	newErr := CatchNewMQPanic()
+	noSslConf := tMqconf
+	noSslConf.Ssl = false
+	noSslConf.VerifyPeer = false
+	noSslConf.Port = 42
+
+	newErr := CatchNewMQPanic(t, noSslConf)
 
 	if newErr == nil {
 		t.Errorf("New MQ did not report error when it should.")
 	}
+}
+
+func CatchTLSConfigBrokerPanic(b Mqconf) (cfg *tls.Config, err error) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			err = fmt.Errorf("Caught panic")
+		}
+	}()
+
+	cfg = TLSConfigBroker(b)
+
+	return cfg, nil
 }
 
 func TestTLSConfigBroker(t *testing.T) {
@@ -142,6 +266,44 @@ func TestTLSConfigBroker(t *testing.T) {
 	notls := TLSConfigBroker(noSslConf)
 
 	assert.Zero(t, notls.Certificates, "Expected warnings were missing")
+
+	sslConf := noSslConf
+	sslConf.Cacert = doesNotExist
+
+	_, err := CatchTLSConfigBrokerPanic(sslConf)
+	assert.NotNil(t, err, "Expected failure was missing")
+
+	sslConf.Cacert = readableFile
+
+	_, _ = CatchTLSConfigBrokerPanic(sslConf)
+	// Should we fail here?
+	//	assert.NotNil(t, err, "Expected failure was missing")
+
+	sslConf.VerifyPeer = true
+	sslConf.Cacert = ""
+	sslConf.ClientKey = doesNotExist
+	sslConf.ClientCert = doesNotExist
+
+	_, _ = CatchTLSConfigBrokerPanic(sslConf)
+	// Should we fail here?
+	//	assert.NotNil(t, err, "Expected failure was missing")
+
+	sslConf.VerifyPeer = true
+	sslConf.Cacert = ""
+	sslConf.ClientKey = doesNotExist
+	sslConf.ClientCert = readableFile
+
+	_, _ = CatchTLSConfigBrokerPanic(sslConf)
+	// Should we fail here?
+	//	assert.NotNil(t, err, "Expected failure was missing")
+
+	sslConf.Cacert = ""
+	sslConf.ClientKey = ""
+	sslConf.ClientCert = ""
+
+	_, _ = CatchTLSConfigBrokerPanic(sslConf)
+	// Should we fail here?
+	//	assert.NotNil(t, err, "Expected failure was missing")
 
 }
 

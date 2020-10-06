03032d2afbf95dea48163080df351f88aad2fe76
Pontus Freyhult <pontus.freyhult@nbis.se>
2020-10-06 11:16:19 +0200

    Fixed bad commands

--- .github/integration/setup/common/22_create_and_encrypt_large_file.sh
+++ .github/integration/setup/common/22_create_and_encrypt_large_file.sh
@@ -5,11 +5,9 @@ cd dev_utils || exit 1
 touch largefile.raw
 shred -s 4G largefile.raw
 
-md5sum /largefile.raw > largefile.raw.md5
+md5sum largefile.raw > largefile.raw.md5
 sha256sum largefile.raw > largefile.raw.sha256
 
-crypt4gh  --recipient_pk dev_utils/c4gh.pub.pem < largefile.raw > largefile.c4gh
+crypt4gh encrypt --recipient_pk c4gh.pub.pem < largefile.raw > largefile.c4gh
 
 rm largefile.raw
-
-

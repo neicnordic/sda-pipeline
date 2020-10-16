d0f60007ec7d01ae0d032da6fce8333de95b9d12
Pontus Freyhult <pontus.freyhult@nbis.se>
2020-10-16 08:24:13 +0200

    Temporarily disable tests that can be expected to take a long time

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -271,16 +271,16 @@ func TestS3Backend(t *testing.T) {
 
 	log.SetOutput(&buf)
 
-	_, err = backend.GetFileSize(s3DoesNotExist)
-	assert.NotNil(t, err, "s3 GetFileSize worked when it should not")
-	assert.NotZero(t, buf.Len(), "Expected warning missing")
+	//	_, err = backend.GetFileSize(s3DoesNotExist)
+	// assert.NotNil(t, err, "s3 GetFileSize worked when it should not")
+	// assert.NotZero(t, buf.Len(), "Expected warning missing")
 
-	buf.Reset()
+	// buf.Reset()
 
-	reader, err = backend.NewFileReader(s3DoesNotExist)
-	assert.NotNil(t, err, "s3 NewFileReader worked when it should not")
-	assert.Nil(t, reader, "Got a non-nil reader for s3")
-	assert.NotZero(t, buf.Len(), "Expected warning missing")
+	// reader, err = backend.NewFileReader(s3DoesNotExist)
+	// assert.NotNil(t, err, "s3 NewFileReader worked when it should not")
+	// assert.Nil(t, reader, "Got a non-nil reader for s3")
+	// assert.NotZero(t, buf.Len(), "Expected warning missing")
 
 	log.SetOutput(os.Stdout)
 

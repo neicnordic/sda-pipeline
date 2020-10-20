1995bfcadcff841895a9aaa70c234bb4dd0fc536
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2020-10-20 10:21:07 +0200

    Update integration test

--- dev_utils/run_integration_test.sh
+++ dev_utils/run_integration_test.sh
@@ -20,20 +20,22 @@ if [ "$1" != "" ]; then
     FILE="$(realpath "$1")"
 fi
 
+file=$(basename "$FILE")
+
 cd "$(dirname "$0")" || exit
 
-s3cmd -c s3cmd.conf put "$FILE" s3://inbox/test/dummy_data.c4gh
+s3cmd -c s3cmd.conf put "$FILE" s3://inbox/test/"$file"
 
-curl -u test:test 'localhost:15672/api/exchanges/test/localega/publish' \
+curl -u test:test 'localhost:15672/api/exchanges/test/sda/publish' \
 -H 'Content-Type: application/json;charset=UTF-8' \
---data-binary '{"vhost":"test","name":"localega","properties":{"delivery_mode":2,"correlation_id":"1","content_encoding":"UTF-8","content_type":"application/json"},"routing_key":"files","payload_encoding":"string","payload":"{\"type\":\"ingest\",\"user\":\"test\",\"filepath\":\"test/dummy_data.c4gh\",\"encrypted_checksums\":[{\"type\":\"sha256\",\"value\":\"'"$SHA"'\",\"type\":\"md5\",\"value\":\"'"$MD5"'\"}]}"}'
+--data-binary '{"vhost":"test","name":"sda","properties":{"delivery_mode":2,"correlation_id":"1","content_encoding":"UTF-8","content_type":"application/json"},"routing_key":"files","payload_encoding":"string","payload":"{\"type\":\"ingest\",\"user\":\"test\",\"filepath\":\"'"$file"'\",\"encrypted_checksums\":[{\"type\":\"sha256\",\"value\":\"'"$SHA"'\",\"type\":\"md5\",\"value\":\"'"$MD5"'\"}]}"}'
 
 RETRY_TIMES=0
 until docker logs --since 30s ingest 2>&1 | grep "Mark as archived"
 do 
     echo "waiting for ingestion to complete"
     RETRY_TIMES=$((RETRY_TIMES+1));
-    if [ $RETRY_TIMES -eq 6 ]; then
+    if [ $RETRY_TIMES -eq 30 ]; then
         echo "Ingest failed"
         exit 1
     fi
@@ -45,14 +47,14 @@ until docker logs --since 30s verify 2>&1 | grep "Mark completed"
 do
     echo "waiting for verification to complete"
     RETRY_TIMES=$((RETRY_TIMES+1));
-    if [ $RETRY_TIMES -eq 6 ]; then
+    if [ $RETRY_TIMES -eq 30 ]; then
         echo "Verification failed"
         exit 1
     fi
     sleep 10;
 done
 
-message=$(curl -u test:test 'localhost:15672/api/queues/test/files.verified/get' \
+message=$(curl -u test:test 'localhost:15672/api/queues/test/verified/get' \
 -H 'Content-Type: application/json;charset=UTF-8' \
 -d '{"count":1,"ackmode":"ack_requeue_true","encoding":"auto","truncate":50000}')
 
@@ -60,18 +62,32 @@ decrypted_sha=$(echo "$message" | jq '.[0].payload | fromjson.decrypted_checksum
 decrypted_md5=$(echo "$message" | jq '.[0].payload | fromjson.decrypted_checksums[1].value' | cut -d '"' -f 2)
 file=$(echo "$message" | jq '.[0].payload | fromjson.filepath' | cut -d '"' -f 2)
 
-curl -u test:test 'localhost:15672/api/exchanges/test/localega/publish' \
+curl -u test:test 'localhost:15672/api/exchanges/test/sda/publish' \
 -H 'Content-Type: application/json;charset=UTF-8' \
---data-binary '{"vhost":"test","name":"localega","properties":{"delivery_mode":2,"correlation_id":"1","content_encoding":"UTF-8","content_type":"application/json"},"routing_key":"stableIDs","payload_encoding":"string","payload":"{\"type\":\"accession\",\"user\":\"test\",\"filepath\":\"'"$file"'\",\"accession_id\":\"EGAF00123456789\",\"decrypted_checksums\":[{\"type\":\"sha256\",\"value\":\"'"$decrypted_sha"'\"},{\"type\":\"md5\",\"value\":\"'"$decrypted_md5"'\"}]}"}'
+--data-binary '{"vhost":"test","name":"sda","properties":{"delivery_mode":2,"correlation_id":"1","content_encoding":"UTF-8","content_type":"application/json"},"routing_key":"files","payload_encoding":"string","payload":"{\"type\":\"accession\",\"user\":\"test\",\"filepath\":\"'"$file"'\",\"accession_id\":\"EGAF00123456789\",\"decrypted_checksums\":[{\"type\":\"sha256\",\"value\":\"'"$decrypted_sha"'\"},{\"type\":\"md5\",\"value\":\"'"$decrypted_md5"'\"}]}"}'
 
 RETRY_TIMES=0
 until docker logs finalize 2>&1 | grep "Mark ready"
 do
     echo "waiting for finalize to complete"
     RETRY_TIMES=$((RETRY_TIMES+1));
-    if [ $RETRY_TIMES -eq 6 ]; then
+    if [ $RETRY_TIMES -eq 30 ]; then
         echo "Finalize failed"
         exit 1
     fi
     sleep 10;
 done
+
+curl -vvv -u test:test 'localhost:15672/api/exchanges/test/sda/publish' \
+-H 'Content-Type: application/json;charset=UTF-8' \
+--data-binary '{"vhost":"test","name":"sda","properties":{"delivery_mode":2,"correlation_id":"1","content_encoding":"UTF-8","content_type":"application/json"},"routing_key":"files","payload_encoding":"string","payload":"{\"type\":\"mapping\",\"dataset_id\":\"EGAD00123456789\",\"accession_ids\":[\"EGAF00123456789\"]}"}'
+
+dataset=$(docker run --rm --name client --network dev_utils_default \
+neicnordic/pg-client:latest postgresql://lega_out:lega_out@db:5432/lega \
+-c "SELECT * from local_ega_ebi.file_dataset" | grep EGAD00123456789)
+if [ ${#dataset} -eq 0 ]; then
+    echo "Mappings failed"
+    exit 1
+else
+    echo "Success"
+fi

12df4732e36011be24e8238bd5b47cb1f946a413
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2021-12-02 08:10:12 +0100

    [8_bad_messages.sh] White space cleanup

--- .github/integration/tests/common/8_bad_messages.sh
+++ .github/integration/tests/common/8_bad_messages.sh
@@ -6,42 +6,39 @@
 # queue and handled again and again.
 #
 
-
-for routingkey in files archived verified completed accessionIDs; do 
-    curl --cacert dev_utils/certs/ca.pem -vvv -u test:test 'https://localhost:15672/api/exchanges/test/sda/publish' \
-	 -H 'Content-Type: application/json;charset=UTF-8' \
-	 --data-binary '{
-                                  "vhost":"test",
-                               	  "name":"sda",
-    	                       	  "properties":{
-    	                                     "delivery_mode":2,
-    	                                     "correlation_id":"1",
-    	                                     "content_encoding":"UTF-8",
-    	                                     "content_type":"application/json"
-    	                                    },
-    	                       "routing_key":"'"$routingkey"'",
-    	                       "payload_encoding":"string",
-    	                       "payload":"{
-			                   I give you bad json!}"
+for routingkey in files archived verified completed accessionIDs; do
+	curl --cacert dev_utils/certs/ca.pem -vvv -u test:test 'https://localhost:15672/api/exchanges/test/sda/publish' \
+		-H 'Content-Type: application/json;charset=UTF-8' \
+		--data-binary '{
+						"vhost":"test",
+						"name":"sda",
+						"properties":{
+							"delivery_mode":2,
+							"correlation_id":"1",
+							"content_encoding":"UTF-8",
+							"content_type":"application/json"
+						},
+						"routing_key":"'"$routingkey"'",
+						"payload_encoding":"string",
+						"payload":"{
+						I give you bad json!}"
 					}'
-					
 done
 
-for routingkey in files archived verified completed accessionIDs; do 
-    curl --cacert dev_utils/certs/ca.pem  -vvv -u test:test 'https://localhost:15672/api/exchanges/test/sda/publish' \
-	 -H 'Content-Type: application/json;charset=UTF-8' \
-	 --data-binary '{
-                                  "vhost":"test",
-                               	  "name":"sda",
-    	                       	  "properties":{
-    	                                     "delivery_mode":2,
-    	                                     "correlation_id":"1",
-    	                                     "content_encoding":"UTF-8",
-    	                                     "content_type":"application/json"
-    	                                    },
-    	                       "routing_key":"'"$routingkey"'",
-    	                       "payload_encoding":"string",
-    	                       "payload":"{ \"json\":\"yes, but not sda\" }"
+for routingkey in files archived verified completed accessionIDs; do
+	curl --cacert dev_utils/certs/ca.pem -vvv -u test:test 'https://localhost:15672/api/exchanges/test/sda/publish' \
+		-H 'Content-Type: application/json;charset=UTF-8' \
+		--data-binary '{
+						"vhost":"test",
+						"name":"sda",
+						"properties":{
+							"delivery_mode":2,
+							"correlation_id":"1",
+							"content_encoding":"UTF-8",
+							"content_type":"application/json"
+						},
+						"routing_key":"'"$routingkey"'",
+						"payload_encoding":"string",
+						"payload":"{ \"json\":\"yes, but not sda\" }"
 					}'
-
 done

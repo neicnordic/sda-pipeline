ec54a4e3eb1f9342553f8fa03e51551253b7dcfd
Alex Aperis <aperisalex@gmail.com>
2022-01-20 16:49:05 +0100

    generate wrong key-pair
    instead of hardcoding a pub key

--- .github/integration/setup/common/22_create_and_encrypt_large_file.sh
+++ .github/integration/setup/common/22_create_and_encrypt_large_file.sh
@@ -20,11 +20,17 @@ dd if=largefile.c4gh bs=1 count=10 of=truncated2.c4gh
 
 dd if=/dev/random of=wrongly_encrypted.raw count=1 bs=$(( 1024 * 1024 *  1 )) iflag=fullblock
 
-cat > wrong_key.pub <<EOF
------BEGIN CRYPT4GH PUBLIC KEY-----
-fGi6kvZlQN37PLmumygbNaSLf0NA+kj4KB1b70HFFCU=
------END CRYPT4GH PUBLIC KEY-----
-EOF
+/usr/bin/expect <<EOD
+spawn crypt4gh-keygen --sk wrong_key.key --pk wrong_key.pub
+match_max 100000
+expect -exact "Generating public/private Crypt4GH key pair.\r
+Enter passphrase for wrong_key.key (empty for no passphrase): "
+send -- "secret\r"
+expect -exact "\r
+Enter passphrase for wrong_key.key (again): "
+send -- "secret\r"
+expect eof
+EOD
 
 crypt4gh encrypt --recipient_pk wrong_key.pub < wrongly_encrypted.raw > wrongly_encrypted.c4gh
-rm wrongly_encrypted.raw wrong_key.pub
+rm -f wrongly_encrypted.raw wrong_key.pub wrong_key.key

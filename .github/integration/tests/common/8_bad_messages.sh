19d928379d93f47b4fc2abdfb8b548a143c2ba9c
Alex Aperis <aperisalex@gmail.com>
2022-03-14 09:40:58 +0100

    enable tests
    - fix cert path
    
    - do not run test for s3notls

--- .github/integration/tests/common/8_bad_messages.sh
+++ .github/integration/tests/common/8_bad_messages.sh
@@ -5,9 +5,11 @@
 # before the "real" work to verify that these failures are not top of
 # queue and handled again and again.
 #
+if [ "$STORAGETYPE" = s3notls ]; then
+    exit 0
+fi
 
-# disable for now
-exit 0
+cd dev_utils || exit 1
 
 function check_move_to_error_queue() {
 	now=$(date -u +%Y-%m-%dT%H:%M:%SZ)
@@ -15,7 +17,7 @@ function check_move_to_error_queue() {
 	RETRY_TIMES=0
 	echo
 	echo "Waiting for msg containing \"""$1""\" to move to error queue."
-	until curl --cacert dev_utils/certs/ca.pem  -u test:test 'https://localhost:15672/api/queues/test/error/get' \
+	until curl --cacert certs/ca.pem  -u test:test 'https://localhost:15672/api/queues/test/error/get' \
 		-H 'Content-Type: application/json;charset=UTF-8' \
 		-d '{"count":1,"ackmode":"ack_requeue_false","encoding":"auto","truncate":50000}' 2>&1 | grep -q "$1"; do
 		printf '%s' "."
@@ -39,7 +41,7 @@ function check_move_to_error_queue() {
 
 #routingkey files requires fixing of #323 first
 for routingkey in ingest archived accessionIDs backup mappings; do
-	curl --cacert dev_utils/certs/ca.pem -vvv -u test:test 'https://localhost:15672/api/exchanges/test/sda/publish' \
+	curl --cacert certs/ca.pem -vvv -u test:test 'https://localhost:15672/api/exchanges/test/sda/publish' \
 		-H 'Content-Type: application/json;charset=UTF-8' \
 		--data-binary '{
 						"vhost":"test",
@@ -61,7 +63,7 @@ check_move_to_error_queue "I give you bad json"
 
 #routingkey files requires fixing of #323 first
 for routingkey in ingest archived accessionIDs backup mappings; do
-	curl --cacert dev_utils/certs/ca.pem -vvv -u test:test 'https://localhost:15672/api/exchanges/test/sda/publish' \
+	curl --cacert certs/ca.pem -vvv -u test:test 'https://localhost:15672/api/exchanges/test/sda/publish' \
 		-H 'Content-Type: application/json;charset=UTF-8' \
 		--data-binary '{
 						"vhost":"test",
@@ -81,4 +83,4 @@ done
 check_move_to_error_queue "yes, but not sda"
 
 # Cleanup queues
-curl --cacert dev_utils/certs/ca.pem  -u test:test -X DELETE 'https://localhost:15672/api/queues/test/error/contents'
+curl --cacert certs/ca.pem  -u test:test -X DELETE 'https://localhost:15672/api/queues/test/error/contents'

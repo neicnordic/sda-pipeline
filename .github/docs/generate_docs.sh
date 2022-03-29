e01a66e0cef698dff9f91de1f3e77df219428ceb
Stefan Negru <stefan@blankdots.com>
2022-03-29 10:27:44 +0300

    more clean-up of the links in the docs

--- .github/docs/generate_docs.sh
+++ .github/docs/generate_docs.sh
@@ -12,6 +12,31 @@ function normalize_url {
     echo "$1" | sed -E 's,([^:]/)/+,\1,g'
 }
 
+function cleanup {
+    find "${GO_DOC_HTML_OUTPUT:-godoc}" -name 'index.html?m=all.html' | while read -r p; do
+        mv "$p" "${p%?m=all.html}"
+    done
+
+    find "${GO_DOC_HTML_OUTPUT:-godoc}" -type f -print0 | xargs -0 sed -i -e 's/%3Fm=all.html//g'
+
+    # this is for deep nested links
+    find "${GO_DOC_HTML_OUTPUT:-godoc}" -type f -print0 | xargs -0 sed -i -e 's/..\/..\/..\/..\/src\/sda-pipeline\/internal/https:\/\/github.com\/neicnordic\/sda-pipeline\/tree\/master\/internal/g'
+
+    find "${GO_DOC_HTML_OUTPUT:-godoc}" -type f -print0 | xargs -0 sed -i -e 's/..\/..\/..\/src\/sda-pipeline\/internal/https:\/\/github.com\/neicnordic\/sda-pipeline\/tree\/master\/internal/g'
+
+    find "${GO_DOC_HTML_OUTPUT:-godoc}" -type f -print0 | xargs -0 sed -i -e 's/%3Fs=[[:digit:]]*:[[:digit:]]*.html//g'
+
+    find "${GO_DOC_HTML_OUTPUT:-godoc}" -type f -print0 | xargs -0 sed -i -e 's/http:\/\/localhost:6060\/pkg/https:\/\/pkg.go.dev/g'
+
+}
+
+function generate_gh_pages {
+    mkdir -p "${GITHUB_WORKSPACE}/gh-pages/pkg/sda-pipeline/"
+    rsync -av "${GO_DOC_HTML_OUTPUT:-godoc}"/pkg/sda-pipeline/ "${GITHUB_WORKSPACE}/gh-pages/pkg/sda-pipeline/"
+    rsync -av "${GO_DOC_HTML_OUTPUT:-godoc}"/lib/ "${GITHUB_WORKSPACE}/gh-pages/lib/"
+}
+
+
 function generate_go_documentation {
     # Go doc
     local URL
@@ -36,7 +61,7 @@ function generate_go_documentation {
 
     # Starting godoc server
     echo "Starting godoc server..."
-    godoc -http="${GO_DOC_HTTP:-localhost:6060}" -templates .github/docs/godoc_templates &
+    godoc -http="${GO_DOC_HTTP:-localhost:6060}" -templates=.github/docs/godoc_templates &
     PID=$!
 
     # Waiting for godoc server
@@ -58,6 +83,12 @@ function generate_go_documentation {
         --no-host-directories \
         "$URL?m=all"
 
+    echo "cleaning up downloaded pages from suffix"
+    cleanup
+
+    echo "generate github pages"
+    generate_gh_pages
+
     # Stop godoc server
     kill -9 "$PID"
     echo "Stopped godoc server"
@@ -65,18 +96,5 @@ function generate_go_documentation {
     
 }
 
-function cleanup {
-    find "${GO_DOC_HTML_OUTPUT:-godoc}" -name 'index.html?m=all.html' | while read -r p; do
-        mv "$p" "${p%?m=all.html}"
-    done
-}
-
-function generate_gh_pages {
-    mkdir -p "${GITHUB_WORKSPACE}/gh-pages/pkg/sda-pipeline/"
-    rsync -av "${GO_DOC_HTML_OUTPUT:-godoc}"/pkg/sda-pipeline/ "${GITHUB_WORKSPACE}/gh-pages/pkg/sda-pipeline/"
-    rsync -av "${GO_DOC_HTML_OUTPUT:-godoc}"/lib/ "${GITHUB_WORKSPACE}/gh-pages/lib/"
-}
 
 generate_go_documentation
-cleanup
-generate_gh_pages

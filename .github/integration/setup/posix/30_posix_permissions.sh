ae5ef871976d10d928da90a2a1c022af5506b730
Stefan Negru <stefan@blankdots.com>
2022-03-31 10:41:33 +0300

    adjust tests to check for inbox cleanup
    also update to sda-mq v1.4.0

--- .github/integration/setup/posix/30_posix_permissions.sh
+++ .github/integration/setup/posix/30_posix_permissions.sh
@@ -3,3 +3,5 @@
 docker run --rm -v dev_utils_archive:/foo alpine sh -c "chmod 777 /foo"
 docker run --rm -v dev_utils_backup:/foo alpine sh -c "chmod 777 /foo"
 
+# we need to have permissions to delete the file
+sudo chmod -R 777 dev_utils/

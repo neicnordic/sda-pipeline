b2743f3346047b2a1bca7b20a63c556ecb847733
dbampalikis <dimitris.bampalikis@gmail.com>
2022-11-28 14:37:37 +0100

    Add sleep to database in finalize error

--- internal/database/db.go
+++ internal/database/db.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"fmt"
 	"hash"
+	"math"
 	"path/filepath"
 	"strings"
 	"time"
@@ -202,6 +203,7 @@ func (dbs *SQLdb) MarkCompleted(file FileInfo, fileID int) error {
 		err = dbs.markCompleted(file, fileID)
 		count++
 	}
+
 	return err
 }
 
@@ -232,6 +234,7 @@ func (dbs *SQLdb) markCompleted(file FileInfo, fileID int) error {
 	if rowsAffected, _ := result.RowsAffected(); rowsAffected == 0 {
 		return errors.New("something went wrong with the query zero rows were changed")
 	}
+
 	return nil
 }
 
@@ -349,10 +352,15 @@ func (dbs *SQLdb) MarkReady(accessionID, user, filepath, checksum string) error 
 		count int   = 0
 	)
 
-	for count == 0 || (err != nil && count < dbRetryTimes) {
+	// 3, 9, 27, 81, 243 seconds between each retry event.
+	for count = 1; count <= dbRetryTimes; count++ {
 		err = dbs.markReady(accessionID, user, filepath, checksum)
-		count++
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Duration(math.Pow(3, float64(count))) * time.Second)
 	}
+
 	return err
 }
 
@@ -380,11 +388,13 @@ func (dbs *SQLdb) MapFilesToDataset(datasetID string, accessionIDs []string) err
 		count int
 	)
 
-	// 0, 3, 9, 27, 81, 243 seconds between each retry event.
-	for count == 0 || (err != nil && count < dbRetryTimes) {
-		time.Sleep(time.Duration(count*3) * time.Second)
+	// 3, 9, 27, 81, 243 seconds between each retry event.
+	for count = 1; count <= dbRetryTimes; count++ {
 		err = dbs.mapFilesToDataset(datasetID, accessionIDs)
-		count++
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Duration(math.Pow(3, float64(count))) * time.Second)
 	}
 
 	return err
